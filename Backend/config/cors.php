3e94b9a6661a879b98d45db3e352e25a03d817a3
Yahia <nouredineyahia77@gmail.com>
2025-12-08 20:58:39 +0100

    add ssome modf

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -26,6 +26,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
+        'https://inscription-cyber.vercel.app/',
     ],
 
     'allowed_origins_patterns' => [],
